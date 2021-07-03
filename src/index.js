9be401633dfe9b45b8ef505b0603a6902baad431
Jonas Boone <jonas.boone@student.kdg.be>
2021-07-03 17:56:19 +0200

    Finalized put and delete request

--- src/index.js
+++ src/index.js
@@ -15,20 +15,30 @@ import EasyHTTP from "./js/app";
 const http = new EasyHTTP();
 
 // Get request all users
-// http.get("https://jsonplaceholder.typicode.com/users")
-//     .then(data => console.log(data))
-//     .catch(e => console.log(`Error message ${e}`));
+http.get("https://jsonplaceholder.typicode.com/users")
+    .then(data => console.log(data))
+    .catch(e => console.log(`Error message ${e}`));
 
 // Post request
 
-    // Create fake user data
+// Create fake user data
 const data = {
     name: "John Doe",
     username: "johndoe",
     email: "johndoe@gmail.com"
 }
 
-    // Post data
+// Post data
 http.post("https://jsonplaceholder.typicode.com/users", data)
     .then(data => console.log(data))
+    .catch(e => console.log(`Error message ${e}`));
+
+// Put request
+http.put("https://jsonplaceholder.typicode.com/users/1", data)
+    .then(data => console.log(data))
+    .catch(e => console.log(`Error message ${e}`));
+
+// Delete request
+http.delete("https://jsonplaceholder.typicode.com/users/1")
+    .then(message => console.log(message))
     .catch(e => console.log(`Error message ${e}`));
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 // Create class for HTTP request
  export default class EasyHTTP {
+    // Make a GET request
     async get(url) {
         try {
             const response = await fetch(url);
@@ -8,7 +9,7 @@
             console.log(`Error ${e}`);
         }
     }
-
+    // Make a POST request
      async post(url, data) {
          try {
              const response = await fetch(url, {
@@ -19,6 +20,35 @@
                  body: JSON.stringify(data)
              });
              return await response.json();
+         } catch (e) {
+             console.log(`Error ${e}`);
+         }
+     }
+    // Make a PUT request
+     async put(url, data) {
+         try {
+             const response = await fetch(url, {
+                 method: "PUT",
+                 headers: {
+                     "Content-type": "application/json"
+                 },
+                 body: JSON.stringify(data)
+             });
+             return await response.json();
+         } catch (e) {
+             console.log(`Error ${e}`);
+         }
+     }
+     // Make a DELETE request
+     async delete(url) {
+         try {
+             const response = await fetch(url, {
+                 method: "DELETE",
+                 headers: {
+                     "Content-type": "application/json"
+                 }
+             });
+             return "Resource deleted!";
          } catch (e) {
              console.log(`Error ${e}`);
          }
